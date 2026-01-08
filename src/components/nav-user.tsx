0a7dc0b0dbfee7c46d3e755a29e29d5334cca56b
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2026-01-08 11:33:05 +0100

    fix: remove grayscale filter from user avatar

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -42,7 +42,7 @@ export function NavUser() {
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <Avatar className="h-8 w-8 rounded-lg grayscale">
+              <Avatar className="h-8 w-8 rounded-lg">
                 <AvatarImage src={user?.profile.email ? gravatarUrl({ email: user.profile.email }) : undefined} alt={user?.profile.name}/>
                 <AvatarFallback className="rounded-lg">CN</AvatarFallback>
               </Avatar>
