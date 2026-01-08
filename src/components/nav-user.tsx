e6ba53d129074c02fc3425041d7f30d7c27541df
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2026-01-08 11:01:55 +0100

    feat: update user avatar to use Gravatar

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -64,7 +64,7 @@ export function NavUser() {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user?.profile.picture} alt={user?.profile.name}/>
+                  <AvatarImage src={user?.profile.email ? gravatarUrl({ email: user.profile.email }) : undefined} alt={user?.profile.name}/>
                   <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
